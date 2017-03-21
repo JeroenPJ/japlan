36c0cd138abf41a3329a856b0c853459ac09bc3c
Jeroen Jagt <jeroenpjagt@gmail.com>
2017-03-21 15:45:27 +0900

    Temporary redirect to wordpress site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :redirect
+
+  def redirect
+    redirect_to "http://japlanblog.wordpress.com/"
+  end
 end
